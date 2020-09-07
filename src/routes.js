f0503dfbeb3d2bacac1bd8e65cfa166be9a534fa
Ifeoluwa Lawal <il693@nyu.edu>
2020-09-07 18:35:13 -0400

    deployed to gh-pages

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React from 'react';
 import {
     BrowserRouter as Router,
     Route
-} from 'react-router-dom'
+} from 'react-router-dom';
 
 import LocationCardList from './components/LocationCardList';
 
